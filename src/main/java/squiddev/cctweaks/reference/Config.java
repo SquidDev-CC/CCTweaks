eeda0e6574a1f19509381f1928b0d120bd57bd4f
SquidDev <bonzoweb@hotmail.co.uk>
2014-10-14 17:22:24 +0100

    Group some config items

--- src/main/java/squiddev/cctweaks/reference/Config.java
+++ src/main/java/squiddev/cctweaks/reference/Config.java
@@ -24,16 +24,15 @@ public final class Config {
 	public static final class ConfigHandler{
 		public static Configuration config;
 
-		private static final String ENABLED = "enabled";
-		private static final String TURTLE_IDS = "turtle_ids";
+		private static final String ITEMS = "items";
 		private static final String MISC = "misc";
 		private static final String COMPUTER = "computer";
 
 		// Description
 		private static final String ENABLE_FORMAT = "Enable the %s";
-		private static final String TURTLE_ID_FORMAT = "The turtle upgrade id of %s";
+		private static final String TURTLE_ID_FORMAT = "The turtle upgrade id of '%s'";
 
-		public static final String[] CATEGORIES = { ENABLED, COMPUTER, TURTLE_IDS, MISC };
+		public static final String[] CATEGORIES = {ITEMS, COMPUTER, MISC };
 		public static final String LANGUAGE_ROOT = "config.cctweaks.";
 
 		public static void init(File file) {
@@ -46,10 +45,10 @@ public final class Config {
 		public static void sync(){
 			// Enabled
 			enableItemComputerUpgrades = getEnabled("Computer Upgrade");
-			enableTurtleToolHost = getEnabled("Turtle Tool Host");
+			enableTurtleToolHost = getEnabled("Tool Host");
 
 			// Turtle Ids
-			turtleToolHostId = getTurtleUpgradeId("Turtle Tool Host", turtleToolHostId);
+			turtleToolHostId = getTurtleUpgradeId("Tool Host ID", turtleToolHostId);
 
 			globalWhitelist = new HashSet<String>(Arrays.asList(config.getStringList("Whitelisted Globals", COMPUTER, new String[0], "Globals to whitelist (are not set to nil)")));
 
@@ -57,11 +56,8 @@ public final class Config {
 			debug = config.getBoolean("debugging", MISC, false, "Is debugging");
 
 			// Pretty comments
-			config.setCategoryComment(ENABLED, "Feature Enabled");
-			config.setCategoryRequiresMcRestart(ENABLED, true);
-
-			config.setCategoryComment(TURTLE_IDS, "Turtle Upgrade Ids");
-			config.setCategoryRequiresMcRestart(TURTLE_IDS, true);
+			config.setCategoryComment(ITEMS, "Items enabled and settings");
+			config.setCategoryRequiresMcRestart(ITEMS, true);
 
 			config.setCategoryComment(COMPUTER, "Tweaks to computer classes");
 			config.setCategoryRequiresMcRestart(COMPUTER, true);
@@ -75,12 +71,12 @@ public final class Config {
 			config.save();
 		}
 		private static boolean getEnabled(String key) {
-			return config.getBoolean(key, ENABLED, true, String.format(ENABLE_FORMAT, key));
+			return config.getBoolean(key, ITEMS, true, String.format(ENABLE_FORMAT, key));
 		}
 
 		private static int getTurtleUpgradeId(String key, int defaultValue)
 		{
-			return config.getInt(key, TURTLE_IDS, defaultValue, 64, 32767, String.format(TURTLE_ID_FORMAT, key));
+			return config.getInt(key, ITEMS, defaultValue, 64, 32767, String.format(TURTLE_ID_FORMAT, key));
 		}
 	}
 }
