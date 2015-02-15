befc4d7e7e1fd6e49e98e62bded3f2b7861a2d5a
SquidDev <bonzoweb@hotmail.co.uk>
2015-02-15 15:43:59 +0000

    Update CoFH API

---
+++ src/api/java/cofh/api/energy/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * (C) 2014 Team CoFH / CoFH / Cult of the Full Hub
+ * http://www.teamcofh.com
+ */
+@API(apiVersion = "1.7.10R1.0.7", owner = "CoFHAPI", provides = "CoFHAPI|energy")
+package cofh.api.energy;
+
+import cpw.mods.fml.common.API;
