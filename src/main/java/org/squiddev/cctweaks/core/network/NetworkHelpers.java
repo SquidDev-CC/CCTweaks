462b0c3eb43afd1746d3175fb46ba60d70c5b177
ElvishJerricco <elvishjerricco@gmail.com>
2015-05-18 13:16:05 -0400

    Removed invalid network helper functions
    Still need to revise the remaining one

--- src/main/java/org/squiddev/cctweaks/core/network/NetworkHelpers.java
+++ src/main/java/org/squiddev/cctweaks/core/network/NetworkHelpers.java
@@ -17,48 +17,6 @@ import java.util.Set;
  */
 public final class NetworkHelpers {
 	/**
-	 * Fire {@link INetworkNode#networkInvalidated()} on current and adjacent nodes
-	 *
-	 * @param world The world the block lies in
-	 * @param x     The X position of the node
-	 * @param y     The Y position of the node
-	 * @param z     The Z position of the node
-	 */
-	public static void fireNetworkInvalidateAdjacent(IBlockAccess world, int x, int y, int z) {
-		Set<ISearchLoc> visited = new HashSet<ISearchLoc>();
-
-		for (ISearchLoc loc : new NetworkVisitorIterable(world, x, y, z, visited)) {
-			loc.getNode().networkInvalidated();
-		}
-
-		for (int dir = 0; dir < 6; dir++) {
-			for (ISearchLoc loc : new NetworkVisitorIterable(
-				world,
-				Facing.offsetsXForSide[dir] + x,
-				Facing.offsetsYForSide[dir] + y,
-				Facing.offsetsZForSide[dir] + z,
-				visited
-			)) {
-				loc.getNode().networkInvalidated();
-			}
-		}
-	}
-
-	/**
-	 * Fire {@link INetworkNode#networkInvalidated()} on the current network
-	 *
-	 * @param world The world the block lies in
-	 * @param x     The X position of the node
-	 * @param y     The Y position of the node
-	 * @param z     The Z position of the node
-	 */
-	public static void fireNetworkInvalidate(IBlockAccess world, int x, int y, int z) {
-		for (ISearchLoc loc : new NetworkVisitorIterable(world, x, y, z)) {
-			loc.getNode().networkInvalidated();
-		}
-	}
-
-	/**
 	 * Check if a block is a cable and can be connected to
 	 *
 	 * @param world     World the node lies in
@@ -75,20 +33,5 @@ public final class NetworkHelpers {
 
 		INetworkNode node = NetworkAPI.registry().getNode(world, x, y, z);
 		return node != null && node.canBeVisited(direction.getOpposite());
-	}
-
-	/**
-	 * Send a packet across the network
-	 *
-	 * @param world  The world the block lies in
-	 * @param x      The X position of the node
-	 * @param y      The Y position of the node
-	 * @param z      The Z position of the node
-	 * @param packet Packet to send
-	 */
-	public static void sendPacket(IBlockAccess world, int x, int y, int z, Packet packet) {
-		for (ISearchLoc loc : new NetworkVisitorIterable(world, x, y, z)) {
-			loc.getNode().receivePacket(packet, loc.getDistance());
-		}
 	}
 }
