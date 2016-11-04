1a7c7b6fb0e2dedfe8a7277d57200129461a3584
SquidDev <bonzoweb@hotmail.co.uk>
2016-11-04 23:08:03 +0000

    Defer synchronization in PeripheralAPI

--- src/main/java/org/squiddev/cctweaks/core/patch/PeripheralAPI_Patch.java
+++ src/main/java/org/squiddev/cctweaks/core/patch/PeripheralAPI_Patch.java
@@ -5,6 +5,7 @@ import dan200.computercraft.api.peripheral.IPeripheral;
 import dan200.computercraft.core.apis.IAPIEnvironment;
 import dan200.computercraft.core.apis.PeripheralAPI;
 import dan200.computercraft.core.computer.Computer;
+import dan200.computercraft.core.filesystem.FileSystem;
 import org.squiddev.cctweaks.api.network.INetworkAccess;
 import org.squiddev.cctweaks.api.network.INetworkedPeripheral;
 import org.squiddev.cctweaks.api.network.Packet;
@@ -12,10 +13,13 @@ import org.squiddev.patcher.visitors.MergeVisitor;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class PeripheralAPI_Patch extends PeripheralAPI {
 	@MergeVisitor.Stub
 	private PeripheralWrapper[] m_peripherals;
+	@MergeVisitor.Stub
+	private FileSystem m_fileSystem;
 
 	@MergeVisitor.Stub
 	public PeripheralAPI_Patch(IAPIEnvironment _environment) {
@@ -28,6 +32,10 @@ public class PeripheralAPI_Patch extends PeripheralAPI {
 		private final String m_side;
 		@MergeVisitor.Stub
 		private final IPeripheral m_peripheral;
+		@MergeVisitor.Stub
+		private boolean m_attached;
+		@MergeVisitor.Stub
+		private Set<String> m_mounts;
 
 		@MergeVisitor.Stub
 		public PeripheralWrapper(IPeripheral peripheral, String side) {
@@ -47,16 +55,20 @@ public class PeripheralAPI_Patch extends PeripheralAPI {
 			}
 		}
 
-		@MergeVisitor.Stub
-		@MergeVisitor.Rename(from = {"detach"})
-		public void nativeDetach() {
-		}
-
-		public synchronized void detach() {
+		public void detach() {
 			if (m_peripheral instanceof INetworkedPeripheral) {
 				((INetworkedPeripheral) m_peripheral).detachFromNetwork(this, m_side);
 			}
-			nativeDetach();
+
+			m_peripheral.detach(this);
+
+			synchronized (this) {
+				m_attached = false;
+				for (String mount : m_mounts) {
+					m_fileSystem.unmount(mount);
+				}
+				m_mounts.clear();
+			}
 		}
 
 		@Override
