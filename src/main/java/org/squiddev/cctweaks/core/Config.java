9346b150f1dbf1e5d26ea8472a9ecb482fc40378
SquidDev <bonzoweb@hotmail.co.uk>
2017-03-11 18:28:34 +0000

    Fix _MC_VERSION constant
    See #153

--- src/main/java/org/squiddev/cctweaks/core/Config.java
+++ src/main/java/org/squiddev/cctweaks/core/Config.java
@@ -17,6 +17,7 @@ public final class Config {
 	public static Set<String> turtleDisabledActions;
 
 	public static void init(File file) {
+		org.squiddev.cctweaks.lua.Config.mcVersion = "${mc_version}";
 		org.squiddev.cctweaks.lua.ConfigForgeLoader.init(file);
 		org.squiddev.cctweaks.core.ConfigForgeLoader.init(org.squiddev.cctweaks.lua.ConfigForgeLoader.getConfiguration());
 	}
